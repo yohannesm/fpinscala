b7dfeed1a0c2df5accbb3997bb9930ab4838db0c
Marcell Himawan <mhimawan@hiya.com>
2019-08-13 12:30:28 -0700

    compiled

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -55,8 +55,6 @@ object Option {
     mean(xs).flatMap(m => mean(xs.map(x => Math.pow(x - m, 2))))
   }
 
-  def variance1(xs: Seq[Double]): Option[Double] = {}
-
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(va => b.map(vb => f(va, vb)))
 
