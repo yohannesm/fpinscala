fcad341c744f0712b372e234da8037003a5fb54d
yohannesm <yohannes.marcell@gmail.com>
2019-09-15 19:45:20 -0700

    implemented zips funcs

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -122,7 +122,26 @@ trait Stream[+A] {
       }
     }
 
-  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] = ???
+  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] =
+    unfold((this, s2)) {
+      case (Cons(h1,t1), Cons(h2,t2)) =>
+        Some((f(h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+
+  def zip[B](s2: Stream[B]): Stream[(A, B)] =
+    zipWith(s2)((_, _))
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
+    zipWithAll(s2)((_, _))
+
+  def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+    unfold((this, s2)) {
+      case (Empty, Empty) => None
+      case (Cons(h1, t1), Empty) => Some(f(Some(h1()), None[B]) -> (t1(), Stream.empty[B]))
+      case (Empty, Cons(h2, t2)) => Some(f(None[A], Some(h2())) -> (Stream.empty[A] -> t2()))
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
+    }
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
